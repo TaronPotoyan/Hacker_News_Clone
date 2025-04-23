1036c239397cdb5601dbf4671a3f326d64905716
Taron <potoyantaron@gmail.com>
2025-04-23 23:27:09 +0400

    [UPDATE](back-end and front-end)

---
+++ backend/controller/rout_login.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { Validation_pass, Validation_name } from '../midlwear/passwordval.js';
+import userrout from '../service/controller_User.js';
+
+
+const route = express.Router();
+
+route.post('/', Validation_name, Validation_pass, userrout.GetUser);
+
+route.post('/create', Validation_name, Validation_pass, userrout.CreatUser);
+
+
+export default route;
